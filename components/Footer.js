c927350feb2f83414c769a8be45198f64716be93
Samy <dev.exelight@gmail.com>
2022-02-16 17:41:50 +0100

    .

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,12 @@
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div className="text-center h-32 flex items-center justify-center">
-      <p className="text-xl text-violet-700">&copy; Copyright 2022</p>
+    <div className="text-center h-32 flex flex-col items-center justify-center ">
+      <Link href="/contact" passHref>
+        <button className="bg-violet-600 font-bold text-white py-3 mb-5 px-6 rounded text-sm mt-4">CONTACT US </button>
+      </Link>
+      <p className="text-xl text-violet-700 mb-3">&copy; Copyright 2022</p>
     </div>
   )
 }
--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import Link from "next/link"
 
 const Hero = () => {
   return (
@@ -9,9 +8,6 @@ const Hero = () => {
       </div>
       <h1 className="text-2xl font-bold text-gray-700 uppercase"> Next Movie </h1>
       <p className="text-gray-500"> A simple movie search App </p>
-      <Link href="/contact" passHref>
-        <button className="bg-violet-600 font-bold text-white py-3 px-6 rounded text-sm mt-4">CONTACT US </button>
-      </Link>
     </div>
   )
 }
